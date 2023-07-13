80894410a3a76fdbab3c31ed0159749b6ed65483
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-07-14 02:44:17 +0330

    Added

---
+++ components/icons/chevrons-left.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CompChevronsLeft from "public/assets/icon/chevrons-left.svg"
+
+const IconChevronsLeft = (props : {className?:string; style?:React.CSSProperties}) => {
+    return <CompChevronsLeft {...props}/>
+}
+
+export default IconChevronsLeft
---
+++ components/icons/gear-stroke.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CompGearStroke from "public/assets/icon/gear-stroke.svg"
+
+const IconGearStroke = (props : {className?:string; style?:React.CSSProperties}) => {
+    return <CompGearStroke {...props}/>
+}
+
+export default IconGearStroke
