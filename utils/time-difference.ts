ba57e0b5a3b3993f530807fa2ea8cf102fc0defd
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-07 13:21:02 +0200

    Added

---
+++ utils/time-difference.ts
@@ -0,0 +1,11 @@
+const timeDifference = (startDate: Date, endDate?: Date) => {
+    const currentDate = new Date();
+
+    const timeDiff = (endDate ?? currentDate).getTime() - startDate.getTime();
+
+    const daysPassed = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    return daysPassed;
+};
+
+export default timeDifference;
