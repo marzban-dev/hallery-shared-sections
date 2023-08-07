9d9cbb72af76092369b8099667939d5704eee885
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-07 13:33:32 +0200

    Updated

--- hooks/use-art.ts
+++ hooks/use-art.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getArt } from "apis/arts.api";
+import { getArt } from "shared/apis/arts.api";
 
 export const useArtQuery = (id: number) => {
     return useQuery(["art", id], ({ signal }) => getArt({ id, signal }));
