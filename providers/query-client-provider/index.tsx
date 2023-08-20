4e59cc6780747c068d8286251a38472ab45d2ebf
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-20 15:43:01 +0200

    Remove persister

--- providers/query-client-provider/index.tsx
+++ providers/query-client-provider/index.tsx
@@ -1,18 +1,10 @@
 "use client";
 
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider as QueryProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { PersistQueryClientOptions, PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const QueryClientProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [persisterStorage, setPersisterStorage] = useState<Storage | undefined>(undefined);
-
-    useEffect(() => {
-        setPersisterStorage(window.localStorage);
-    }, []);
-
     const [client] = useState(
         new QueryClient({
             defaultOptions: {
@@ -24,27 +16,12 @@ const QueryClientProvider: React.FC<{ children: React.ReactNode }> = ({ children
         })
     );
 
-    const persister = createSyncStoragePersister({ storage: persisterStorage });
-
-    const persistQueries: string[] = ["explore"];
-
-    const persistOptions: Omit<PersistQueryClientOptions, "queryClient"> = {
-        persister,
-        maxAge: 1000 * 60 * 60 * 24,
-        hydrateOptions: undefined,
-        dehydrateOptions: {
-            shouldDehydrateQuery: ({ queryKey }) => {
-                return persistQueries.includes(queryKey[0] as string);
-            },
-        },
-    };
-
-    return persisterStorage ? (
-        <PersistQueryClientProvider client={client} persistOptions={persistOptions}>
+    return (
+        <QueryProvider client={client}>
             {children}
             <ReactQueryDevtools initialIsOpen={false} />
-        </PersistQueryClientProvider>
-    ) : null;
+        </QueryProvider>
+    );
 };
 
 export default QueryClientProvider;
