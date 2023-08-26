8b8fc85ed92089748399399e40cf90886e6a6de0
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-26 13:43:27 +0200

    Updated

---
+++ components/icons/lock-keyhole.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CompLockKeyhole from "shared/components/icons/svgs/lock-keyhole.svg"
+
+const IconLockKeyhole = (props : {className?:string; style?:React.CSSProperties}) => {
+    return <CompLockKeyhole {...props}/>
+}
+
+export default IconLockKeyhole
