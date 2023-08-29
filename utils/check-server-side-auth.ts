3592485d1937e20246e9655f2f06c066b83e4b10
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-29 12:59:58 +0200

    Create two separate func for hallery and hallery-artist server side auth

--- utils/check-server-side-auth.ts
+++ utils/check-server-side-auth.ts
@@ -1,11 +1,31 @@
 import { authOptions } from "shared/config/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { checkTokenValidity } from "shared/apis/auth.api";
 
-export const checkServerSideAuth = async () => {
+const checkAuth = async () => {
     const session = await getServerSession(authOptions);
+    const isTokenValid = await checkTokenValidity();
 
-    if (!session) {
+    return { session, isTokenValid };
+};
+
+export const checkServerAuth = async () => {
+    const { isTokenValid, session } = await checkAuth();
+
+    if (!session || !isTokenValid) {
         redirect("/auth/signin");
+    }
+};
+
+export const checkDashboardServerAuth = async () => {
+    const { isTokenValid, session } = await checkAuth();
+
+    if (!session || !isTokenValid) {
+        redirect("/auth/signin");
+    }
+
+    if (session && !session.user.artist) {
+        redirect("/become-an-artist");
     }
 };
