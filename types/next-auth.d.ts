1380831fbe2f47ea8703bac7a5810790b0b14aea
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-09-02 15:00:39 +0200

    Updated

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -6,6 +6,7 @@ declare module "next-auth" {
         user: IUser;
         accessToken: string;
         refreshToken: string;
+        tokenExpiry: number;
         expires: DefaultSession["expires"];
     }
 
