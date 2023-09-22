48ae57c8197bbe546bcb31a2f3ce4cdba869b336
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-09-22 16:22:41 +0200

    Improve navbar ui in mobile

--- components/navbar-container/index.tsx
+++ components/navbar-container/index.tsx
@@ -3,8 +3,8 @@ import { INavbarContainerProps } from "./navbar-container.types";
 
 const NavbarContainer: React.FC<INavbarContainerProps> = ({ children }) => {
     return (
-        <nav className="bottom-[20px] z-50 flex w-full items-start justify-center max-[800px]:fixed min-[800px]:h-screen min-[800px]:w-[100px] min-[1000px]:w-[250px]">
-            <ul className="flex gap-2 rounded-full max-[800px]:bg-[rgba(20,20,20,0.8)] max-[800px]:px-4 max-[800px]:backdrop-blur-xl min-[420px]:gap-5 min-[800px]:flex-col min-[800px]:pt-[40px]">
+        <nav className="bottom-0 z-50 flex w-full items-start justify-center max-[800px]:fixed min-[430px]:bottom-[20px] min-[800px]:h-screen min-[800px]:w-[100px] min-[1000px]:w-[250px]">
+            <ul className="flex gap-2 max-[800px]:bg-[rgba(20,20,20,0.8)] max-[800px]:px-4 max-[800px]:backdrop-blur-xl max-[430px]:w-full max-[430px]:justify-center max-[430px]:border-t max-[430px]:border-[rgb(50,50,50)] min-[420px]:gap-5 min-[430px]:rounded-full min-[800px]:flex-col min-[800px]:pt-[40px]">
                 <div className="mb-[20px] flex w-full items-center justify-center max-[800px]:hidden">
                     <Logo />
                 </div>
