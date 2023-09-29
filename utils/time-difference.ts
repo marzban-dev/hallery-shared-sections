5ab9998c47dc44d74b0d1c6a191188e9b876243a
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-09-29 14:39:52 +0200

    Update time-diff func

--- utils/time-difference.ts
+++ utils/time-difference.ts
@@ -1,11 +1,13 @@
-const timeDifference = (startDate: Date, endDate?: Date) => {
+const timeDifference = (startDate: Date, endDate?: Date, humanReadable?: boolean) => {
     const currentDate = new Date();
 
     const timeDiff = (endDate ?? currentDate).getTime() - startDate.getTime();
 
     const daysPassed = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
-    return daysPassed;
+    if (humanReadable) {
+        return daysPassed === 0 ? "Today" : `${daysPassed} days ago`;
+    } else return daysPassed;
 };
 
 export default timeDifference;
