1493ef5aae31a5d0c21fe3904e731df48ff92023
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-10-26 16:09:37 +0200

    Change chart state to (Coming soon...)

---
+++ components/icons/empty-chart.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CompEmptyChart from "shared/components/icons/svgs/empty-chart.svg"
+
+const IconEmptyChart = (props : {className?:string; style?:React.CSSProperties}) => {
+    return <CompEmptyChart {...props}/>
+}
+
+export default IconEmptyChart
